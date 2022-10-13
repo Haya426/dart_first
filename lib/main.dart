8f929d3ea8bb7cb21f6abec22cba3d09bec92aba
MB Phong Ram <mabuphongram171@gmail.com>
2022-10-13 09:53:14 -0400

    Creating home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,46 @@
 // Import the generated file
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:learningdart/views/login_view.dart';
+import 'package:flutter/material.dart';
+import 'package:learningdart/firebase_options.dart';
+import 'package:learningdart/views/register_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const RegisterView()
-    );
-  }
+      home: const HomePage(), 
+    
+  ),
+  );
 }
- class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-      ),
+      appBar: AppBar(title: const Text('Home Page'),),
       body: FutureBuilder(
-        future: Firebase.initializeApp(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context,snapshot){
+          switch(snapshot.connectionState){
             
             case ConnectionState.done:
-         return Column(
-            children: [
-              TextField(
-                controller: _email,
-                enableSuggestions: true,
-                autocorrect: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration:
-                    const InputDecoration(hintText: 'Enter your email here'),
-              ),
-              TextField(
-                controller: _password,
-                autocorrect: false,
-                obscureText: true,
-                enableSuggestions: false,
-                decoration:
-                    const InputDecoration(hintText: 'Enter your password here'),
-              ),
-              TextButton(
-                onPressed: () async {
-                  final email = _email.text;
-                  final password = _password.text;
-                  try {
-                    await FirebaseAuth.instance
-                      .createUserWithEmailAndPassword(
-                          email: email, password: password);
-                  } on FirebaseAuthException catch (e){
-                   if(e.code == 'weak-password'){
-                    print('weak password');
-                   }
-                   else if(e.code == 'email-already-in-use'){
-                    print('This email is already in use');
-                   }
-
-                  }
-                },
-                child: const Text('Register'),
-              ),
-            ],
-          );
-            default: return const Text('loading......');
+              return const Text('Done');              
+            default : return const Text('Loading.... ');
           }
-
-          
         },
       ),
     );
   }
 }
 
+
+
+ 
